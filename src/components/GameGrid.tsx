70b8c18e77de8a0debee8718d4d054d3ddaf70e9
bjorndonald <bjorndonaldb@yahoo.com>
2023-06-08 21:39:42 +0100

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,17 +3,20 @@ import React, { useEffect, useState } from 'react'
 import useGames from '../hooks/useGames';
 import apiClient from '../services/api-client';
 import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
 
 
 
 const GameGrid = () => {
-    const { games, error } = useGames()
+    const { games, error, isLoading } = useGames()
+    const skeletons = [1, 2, 3, 4, 5, 6]
 
     return (
         <>
             {error && <p>Error occured</p>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding={10}>
-                {games.map(game => <GameCard game={game} />)}
+                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
+                {!isLoading && games.map(game => <GameCard game={game} />)}
             </SimpleGrid>
         </>
 
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -24,20 +24,22 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([])
   const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     const controller = new AbortController();
     apiClient.get<FetchGamesResponse>('/games')
-      .then(res => setGames(res.data.results))
+      .then(res => { setGames(res.data.results); setIsLoading(false) })
       .catch(err => {
         if (err instanceof CanceledError) return
         setError(err.message)
+        setIsLoading(false)
       })
 
     return () => {
       controller.abort()
     }
   }, [])
-  return { games, error }
+  return { games, error, isLoading }
 }
 
 export default useGames
