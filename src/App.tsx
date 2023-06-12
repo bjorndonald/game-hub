86af6613f0e83d7eb43c6bfc8a3dc095384c2205
bjorndonald <bjorndonaldb@yahoo.com>
2023-06-12 13:54:57 +0100

    Refactor - manage state with zustand

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,10 @@ import SortSelector from './components/SortSelector'
 import { Genre } from './hooks/useGenres'
 import { Platform } from './hooks/usePlatforms'
 
-export interface GameQuery {
-  genreId?: number;
-  platformId?: number;
-  sortOrder: string,
-  searchText: string,
-  pageSize: number,
-}
-
 const pageSize = 15
 
 function App() {
-  const [gameQuery, setGameQuery] = useState<GameQuery>({ pageSize } as GameQuery)
+
   return (
     <Grid templateAreas={{
       base: `"nav" "main"`,
@@ -32,26 +24,24 @@ function App() {
         lg: '200px 1fr'
       }}
     >
-      <GridItem area={"nav"}><NavBar onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })} /></GridItem>
+      <GridItem area={"nav"}><NavBar /></GridItem>
       <Show above="lg">
         <GridItem area={"aside"} paddingX={5}>
-          <GenreList selectedGenreId={gameQuery.genreId}
-            onSelectGenre={(genreId) => setGameQuery({ ...gameQuery, genreId })} />
+          <GenreList />
         </GridItem>
       </Show>
 
       <GridItem area={"main"}>
         <Box marginLeft={2}>
-          <GameHeading gameQuery={gameQuery} />
+          <GameHeading />
           <Flex marginBottom={5}>
             <Box marginRight={5}>
-              <PlatformSelector selectedPlatformId={gameQuery.platformId}
-                onSelectPlatform={(platformId) => setGameQuery({ ...gameQuery, platformId })} />
+              <PlatformSelector />
             </Box>
-            <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={sortOrder => setGameQuery({ ...gameQuery, sortOrder })} />
+            <SortSelector />
           </Flex>
         </Box>
-        <GameGrid gameQuery={gameQuery} />
+        <GameGrid />
       </GridItem>
     </Grid>
   )
